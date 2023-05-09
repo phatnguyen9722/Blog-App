1c46e1483eb4ee5dc75a4460807ccbb87831caa6
Phat Nguyen <phatnguyen.9722@gmail.com>
2023-05-09 16:39:31 +0700

    create basic components

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -6,14 +6,18 @@ import {
 } from "react-router-dom";
 
 import Login from "./pages/Login"
+import Footer from "./components/Footer"
+import Navbar from "./components/Navbar";
 function App() {
   const router = createBrowserRouter([
     {
       path: "/",
       element: (
         <div>
+          <Navbar/>
           <h1>Home page</h1>
           <Link to="about">About Us</Link>
+          <Footer/>
         </div>
       ),
     },
