85523f8a486b6c50b8070f728b1bc245306f00be
Phat Nguyen <phatnguyen.9722@gmail.com>
2023-05-09 15:35:19 +0700

    frontend router basic config

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,31 @@
+import {
+  createBrowserRouter,
+  RouterProvider,
+  Route,
+  Link,
+} from "react-router-dom";
 
-
+import Login from "./pages/Login"
 function App() {
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      element: (
+        <div>
+          <h1>Home page</h1>
+          <Link to="about">About Us</Link>
+        </div>
+      ),
+    },
+    {
+      path: "/login",
+      element: <Login/>,
+    },
+  ]);
+
   return (
     <div className="App">
-      <h1>Hello</h1>
+      <RouterProvider router={router} />
     </div>
   );
 }
