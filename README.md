4f019a617dc66198c0ea8684b6ccddbf6d3b6d25
Phat Nguyen <phatnguyen.9722@gmail.com>
2023-05-10 16:52:21 +0700

    logo and link

--- README.md
+++ README.md
@@ -1,9 +1,20 @@
 ## This is blog app project
+
 ### Using techs:
+
 #### Frontend:
+
 - React.js </br>
 - Router: react router </br>
-#### Backend: 
-- Node.js </br> 
-#### Database: 
+
+#### Backend:
+
+- Node.js </br>
+
+#### Database:
+
 - MySQL </br>
+
+##### Vscode settings:
+
+- That is added for me to practise with vscode, can remove it depend on IDE working on</br>
--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,11 +1,42 @@
 import React from 'react';
-
+import { Link } from 'react-router-dom';
+import Logo from '../asset/img/logo.png';
 function Navbar() {
     return (
         <div className="navbar">
             <div className="container">
-                <div className="logo">logo</div>
-                <div className="links">links</div>
+                <div className="logo">
+                    <Link to="/">
+                        <img src={Logo} alt="Logo Here" />
+                    </Link>
+                </div>
+                <div className="links">
+                    <Link className="link" to="/?cat=art">
+                        <h6>Art</h6>
+                    </Link>
+                    <Link className="link" to="/?cat=art">
+                        <h6>Science</h6>
+                    </Link>
+                    <Link className="link" to="/?cat=art">
+                        <h6>Tech</h6>
+                    </Link>
+                    <Link className="link" to="/?cat=art">
+                        <h6>Cinema</h6>
+                    </Link>
+                    <Link className="link" to="/?cat=art">
+                        <h6>Design</h6>
+                    </Link>
+                    <Link className="link" to="/?cat=art">
+                        <h6>Food</h6>
+                    </Link>
+                    <span>Phat Nguyen</span>
+                    <span>Logout</span>
+                    <span className="write">
+                        <Link className="link" to="/write">
+                            Write
+                        </Link>
+                    </span>
+                </div>
             </div>
         </div>
     );
