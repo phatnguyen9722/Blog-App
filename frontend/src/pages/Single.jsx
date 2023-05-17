8377dd80e0881f9db0d7e19a23de8e5786f76b72
Phat Nguyen <phatnguyen.9722@gmail.com>
2023-05-17 14:06:23 +0700

    single page

--- frontend/src/pages/Single.jsx
+++ frontend/src/pages/Single.jsx
@@ -1,7 +1,24 @@
 import React from 'react';
 
 function Single() {
-    return <div>Single</div>;
+    return (
+        <div className="single">
+            <div className="content">
+                <img src="" alt="This is content img" />
+
+                <div className="user">
+                    <img src="" alt="This is user img" />
+
+                    <div className="info">
+                        <span>Phat Nguyen</span>
+                        <p>Posted 24 hours ago</p>
+                    </div>
+                </div>
+            </div>
+
+            <div className="menu">menu</div>
+        </div>
+    );
 }
 
 export default Single;
